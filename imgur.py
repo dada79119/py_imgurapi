4ee9c9b4e32c57dfa2af2bd56ef4f29093b79c61
ds3 <dada@gmail.com>
2017-07-19 06:58:52 +0000

    second commits

--- imgur.py
+++ imgur.py
@@ -1,13 +1,15 @@
 from imgurpython import ImgurClient
 import random
-client_id = 'your client id'
-client_secret = 'your client secret'
-album_id = 'your album id'
 
-client = ImgurClient(client_id, client_secret)
-images = client.get_album_images(album_id)
-index = random.randint(0, len(images) - 1)
-url = images[index].link.replace('http', 'https')
+def imgurget():
+    client_id = 'your client id'
+    client_secret = 'your client secret'
+    album_id = 'your album id'
+    client = ImgurClient(client_id, client_secret)
+    images = client.get_album_images(album_id)
+    index = random.randint(0, len(images) - 1)
+    url = images[index].link.replace('http', 'https')
+    return url
 
-print(url)
+print(imgurget())
 
